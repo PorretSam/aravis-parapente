a13edf2732f525b14c39a27fe45afd68a69fa0f5
PorretSam <samuelporret@gmail.com>
2025-06-26 17:52:37 +0200

    Update enregistrer.php

--- enregistrer.php
+++ enregistrer.php
@@ -1,1 +1,27 @@
+<?php
+$codeSecret = 'arvis';
+
+if ($_POST['code'] !== $codeSecret) {
+  echo json_encode(['success' => false, 'message' => 'Code incorrect']);
+  exit;
+}
+
+$titre = strip_tags($_POST['titre']);
+$commentaire = strip_tags($_POST['commentaire']);
+$image = $_POST['image'];
+$date = date('d/m/Y');
+
+$newArticle = [
+  'titre' => $titre,
+  'commentaire' => $commentaire,
+  'image' => $image,
+  'date' => $date
+];
+
+$file = 'articles.json';
+$articles = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+$articles[] = $newArticle;
+file_put_contents($file, json_encode($articles, JSON_PRETTY_PRINT));
+
+echo json_encode(['success' => true, 'articles' => $articles]);
 
